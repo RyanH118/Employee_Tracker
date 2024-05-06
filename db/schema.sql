e5cf1f8be1393bd51450911dcc62965c7a1d009e
ryanh <ryanhinson118@mail.com>
2024-05-05 20:53:42 -0400

    Added schema.sql db and tables.

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employees;
+
+CREATE DATABASE employees;
+
+\c employees
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
