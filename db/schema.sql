ab12024bf1785b52387463ef4def8835ca4a71fb
ryanh <ryanhinson118@mail.com>
2024-05-05 20:57:18 -0400

    quick add of not null to schema.

--- db/schema.sql
+++ db/schema.sql
@@ -12,8 +12,8 @@ CREATE TABLE department (
 CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INTEGER,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
   FOREIGN KEY (role_id) REFERENCES role(id)
 );
